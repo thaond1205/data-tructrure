9e0d39de1431452d30911bf5dd11dbd4f87c2098
thao.nguyen15 <thao.nguyen15@onemount.com>
2022-01-02 21:19:48 +0700

    Reverse Integer

---
+++ src/learn/ReverseInteger.java
@@ -0,0 +1,25 @@
+package learn;
+
+public class ReverseInteger {
+
+    // đảo ngược số
+    // input -342784729
+    // output -927487243
+
+    public static void main(String[] args) {
+        int x = -342784729;
+        StringBuilder beforRevert = new StringBuilder();
+        StringBuilder affterRevert = new StringBuilder();
+        beforRevert.append(String.valueOf(x));
+
+        for (int i = beforRevert.length() - 1; i >= 0; i--) {
+            if (i == beforRevert.length() - 1) {
+                affterRevert.append(beforRevert.charAt(0));
+            }
+            if (i > 0) {
+                affterRevert.append(beforRevert.charAt(i));
+            }
+        }
+        System.out.println(Integer.parseInt(String.valueOf(affterRevert)));
+    }
+}
